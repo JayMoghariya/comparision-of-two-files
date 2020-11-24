718c2cd302deb1c05aa5c1bca4fefb7d56dc7c5e
Jay Moghariya <moghariyajay@gmail.com>
2020-11-24 14:10:27 +0530

    Update checker1.py

--- checker1.py
+++ checker1.py
@@ -1,1 +1,18 @@
-
+file=open("enter first file address","r+")
+file1=open("input second file address","r+")
+a=file.readlines()
+b=file1.readlines()
+n=len(a)
+m=len(b)
+if (m>=n):
+    for i in range(n):
+        if ((a[i])!=(b[i])):
+            print(str(i+1)+"  "+a[i]+"  "+b[i])
+        
+else:
+    for i in range(m):
+        if ((a[i])!=(b[i])):
+            print(str(i+1)+"  "+a[i]+"  "+b[i])
+            
+            
+#use "C:\\Users\\Jay Moghariya\\Desktop\\input.txt" this kind of address;  if you put "\" instead of "\\" it will give error .
